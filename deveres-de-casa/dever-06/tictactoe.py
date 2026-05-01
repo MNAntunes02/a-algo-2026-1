ebe19478f144bc4dc86067b7dd46c5be247c18f9
Alisson Antunes <alissonam17@gmail.com>
2026-05-01 13:07:18 -0300

    feat: implementar funções de avaliação do jogo da velha

--- deveres-de-casa/dever-06/tictactoe.py
+++ deveres-de-casa/dever-06/tictactoe.py
@@ -48,3 +48,44 @@ def result(board, action):
     new_board[i][j] = player(board)
     return new_board
 
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    # Check rows and columns
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] is not EMPTY:
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] is not EMPTY:
+            return board[0][i]
+
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] is not EMPTY:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] is not EMPTY:
+        return board[0][2]
+
+    return None
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    if winner(board) is not None:
+        return True
+    # Check for any empty cell
+    return all(board[i][j] != EMPTY for i in range(3) for j in range(3))
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    w = winner(board)
+    if w == X:
+        return 1
+    elif w == O:
+        return -1
+    return 0
